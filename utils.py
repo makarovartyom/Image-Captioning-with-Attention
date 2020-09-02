3b572e9ec9bc12e5b56f0c3bf0425a878289a8df
Artyom Makarov <makarovartyom.ma@gmail.com>
2020-09-02 23:02:42 +0300

    Class for model results visualization

--- utils.py
+++ utils.py
@@ -28,3 +28,69 @@ def get_hypothesis(terms_idx, data_loader):
         words = [word for word in words if word not in (',', '.', '<end>')]
         hypothesis_list.append(words)
     return hypothesis_list
+
+class GetStats:
+    """Class for getting statistics ffrom text training/validation files"""
+    def __init__(self, log_train_path, log_vali_path, bleu_path):
+        self.log_train_path = log_train_path
+        self.log_valid_path = log_valid_path
+        self.bleu_path = bleu_path
+        assert log_train_path == 'training_log.txt', "file must contain training logs and have a name 'training_log.txt'"
+        assert log_valid_path == 'validation_log.txt', "file must contain validation logs and a name 'validation_log.txt'"
+        assert bleu_path == 'bleu.txt', "file must contain bleu scores and have a name 'bleu.txt'"
+
+        train_file = open(log_train_path, "r")
+        train_logs = train_file.readlines()
+        train_file.close()
+
+        valid_file = open(log_valid_path, "r")
+        valid_logs = valid_file.readlines()
+        valid_file.close()
+
+        bleu_file = open("bleu.txt", "r")
+        bleu_score = bleu_file.readlines()
+        bleu_file.close()
+
+    def get_train_loss(self):
+        """Returns training log from training_log.txt file"""
+        losses=[]
+        perplex=[]
+        #assert self.log_file_path == 'training_log.txt', "Outputs training log only. For vailidation loss or BLEU score use get_valid_loss and get_bleu."
+        for line in self.train_logs:
+            loss = re.search('Loss train: (.*), Perplexity train:', line).group(1)
+            losses.append(loss)
+            perp = re.search('Perplexity train: (.*)\n', line).group(1)
+            perplex.append(perp)
+        return lossses, perplex
+
+    def get_valid_log(self):
+        """Returns validation log from validation_log.txt file"""
+        losses = []
+        perplex =[]
+        for line in self.valid_logs:
+            loss = re.search('Loss valid: (.*), Perplexity valid:', line).group(1)
+            losses.append(loss)
+            perp = re.search('Perplexity valid: (.*)\n', line).group(1)
+            perplex.append(perp)
+        return losses, perplex
+
+    def get_bleu(self):
+        """Returns BLEU scores from the text file"""
+        bleu_1_scores=[]
+        bleu_2_scores=[]
+        bleu_3_scores=[]
+        bleu_4_scores=[]
+
+        for line in self.bleu_score:
+            bleu_1 = re.search('BLEU-1: (.*), BLEU-2:', line).group(1)
+            bleu_1_scores.append(bleu_1)
+
+            bleu_2 = re.search('BLEU-2: (.*), BLEU-3:', line).group(1)
+            bleu_2_scores.append(bleu_2)
+
+            bleu_3 = re.search('BLEU-3: (.*), BLEU-4:', line).group(1)
+            bleu_3_scores.append(bleu_3)
+
+            bleu_4 = re.search('BLEU-4: (.*)\n', line).group(1)
+            bleu_4_scores.append(bleu_4)
+        return bleu_1_scores, bleu_2_scores, bleu_3_scores, bleu_4_scores
