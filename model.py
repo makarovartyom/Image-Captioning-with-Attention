cd4327be1279f8406ef670e1bb197a816f3328f5
Artyom Makarov <makarovartyom.ma@gmail.com>
2020-10-19 19:30:05 +0300

    Decoder and initialize hidden function added

--- model.py
+++ model.py
@@ -9,7 +9,7 @@ device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
 
 class EncoderCNN(nn.Module):
-    "Encoder inputs images and returns feature mapps"
+    """Encoder inputs images and returns feature maps"""
     def __init__(self):
         super(EncoderCNN, self).__init__()
         resnet = models.resnet152(pretrained=True)
@@ -29,6 +29,89 @@ class EncoderCNN(nn.Module):
        
         return features
 
+class DecoderRNN(nn.Module):
 
+    """Arguments:
+         - embedding_dim - specified size of embeddings;
+         - hidden_dim - the size of RNN layer (number of hidden states)
+         - vocab_size - size of vocabulary 
+    """
+    def __init__(self, num_features, embedding_dim, hidden_dim, vocab_size, p =0.5):
 
+        super(DecoderRNN, self).__init__()
+        
+        self.num_features = num_features
+        self.embedding_dim = embedding_dim
+        self.hidden_dim = hidden_dim
+        self.vocab_size = vocab_size
+        # scale the inputs to softmax
+        self.sample_temp = 0.5 
+        
+        # embedding layer that turns words into a vector of a specified size
+        self.embeddings = nn.Embedding(vocab_size, embedding_dim)
+        # LSTM will have a single layer of size 512 (512 hidden units)
+        # it will input concatinated context vector (produced by attention) 
+        # and corresponding hidden state of Decoder
+        self.lstm = nn.LSTMCell(embedding_dim + num_features, hidden_dim)
+        # produce the final output
+        self.fc = nn.Linear(hidden_dim, vocab_size)
+        
+        # add attention layer 
+        self.attention = BahdanauAttention(num_features, hidden_dim)
+        # dropout layer
+        self.drop = nn.Dropout(p=p)
+        # add initialization fully-connected layers
+        # initialize hidden state and cell memory using average feature vector 
+        # Source: https://arxiv.org/pdf/1502.03044.pdf
+        self.init_h = nn.Linear(num_features, hidden_dim)
+        self.init_c = nn.Linear(num_features, hidden_dim)
+
+    def forward(self, captions, features, sample_prob = 0.0):
+         """Arguments:
+         - captions - image captions;
+         - features - features returned from Encoder
+         - sample_prob - use it for scheduled sampling
+         """
+
+        # create embeddings for captions of size (batch, sqe_len, embed_dim)
+        embed = self.embeddings(captions)
+        h, c = self.init_hidden(features)
+        seq_len = captions.size(1)
+        feature_size = features.size(1)
+        batch_size = features.size(0)
+        # these tensors will store the outputs from lstm cell and attention weights
+        outputs = torch.zeros(batch_size, seq_len, self.vocab_size).to(device)
+        atten_weights = torch.zeros(batch_size, seq_len, feature_size).to(device)
+
+        # scheduled sampling for training
+        # we do not use it at the first timestep (<start> word)
+        # but later we check if the probability is bigger than random
+        for t in range(seq_len):
+            sample_prob = 0.0 if t == 0 else 0.5
+            use_sampling = np.random.random() < sample_prob
+            if use_sampling == False:
+                word_embed = embed[:,t,:]
+            context, atten_weight = self.attention(features, h)
+            # input_concat shape at time step t = (batch, embedding_dim + hidden_dim)
+            input_concat = torch.cat([word_embed, context], 1)
+            h, c = self.lstm(input_concat, (h,c))
+            h = self.drop(h)
+            output = self.fc(h)
+            if use_sampling == True:
+                # use sampling temperature to amplify the values before applying softmax
+                scaled_output = output / self.sample_temp
+                scoring = F.log_softmax(scaled_output, dim=1)
+                top_idx = scoring.topk(1)[1]
+                word_embed = self.embeddings(top_idx).squeeze(1) 
+            outputs[:, t, :] = output
+            atten_weights[:, t, :] = atten_weight
+        return outputs, atten_weights
+
+    def init_hidden(self, features):
+
+        """Initializes hidden state and cell memory using average feature vector"""
+        mean_annotations = torch.mean(features, dim = 1)
+        h0 = self.init_h(mean_annotations)
+        c0 = self.init_c(mean_annotations)
+        return h0, c0
 
